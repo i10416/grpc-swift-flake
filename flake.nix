bd191f7a2da187a4724f42f9de50cb8816ab7450
i10416 <ito.yo16uh90616@gmail.com>
2024-06-03 19:41:36 +0900

    tidy(build): use flake-parts instead of flake-utils
    References
    - https://ayats.org/blog/no-flake-utils/
    - https://flake.parts/

--- flake.nix
+++ flake.nix
@@ -1,40 +1,46 @@
 {
   description = "gRPC Swift protoc plugins";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.grpc-swift-src = {
     url = "github:grpc/grpc-swift?ref=refs/tags/1.23.0";
     flake = false;
   };
-  outputs = { self, nixpkgs, flake-utils, grpc-swift-src }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        protobuf = pkgs.protobuf;
-        protoc-gen-swift = import ./protoc-gen-swift/default.nix {
-          inherit pkgs system grpc-swift-src;
+  outputs = inputs@{ self, nixpkgs, flake-parts, grpc-swift-src }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { pkgs, system, ... }:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          protobuf = pkgs.protobuf;
+          protoc-gen-swift = import ./protoc-gen-swift/default.nix {
+            inherit pkgs system grpc-swift-src;
+          };
+          protoc-gen-grpc-swift = import ./protoc-gen-grpc-swift/default.nix {
+            inherit pkgs system grpc-swift-src;
+          };
+        in rec {
+          packages = {
+            protoc-gen-swift = protoc-gen-swift;
+            protoc-gen-grpc-swift = protoc-gen-grpc-swift;
+            default = packages.protoc-gen-swift;
+          };
+          devShells = {
+            default = pkgs.mkShell {
+              name = "grpc-swift-shell";
+              buildInputs = with pkgs; [
+                buf
+                git
+                curl
+                unzip
+                iconv
+                protobuf
+                protoc-gen-swift
+                protoc-gen-grpc-swift
+              ];
+            };
+          };
         };
-        protoc-gen-grpc-swift = import ./protoc-gen-grpc-swift/default.nix {
-          inherit pkgs system grpc-swift-src;
-        };
-      in rec {
-        packages = {
-          protoc-gen-swift = protoc-gen-swift;
-          protoc-gen-grpc-swift = protoc-gen-grpc-swift;
-          default = packages.protoc-gen-swift;
-        };
-        devShell = pkgs.mkShell {
-          name = "grpc-swift-shell";
-          buildInputs = with pkgs; [
-            buf
-            git
-            curl
-            unzip
-            iconv
-            protobuf
-            protoc-gen-swift
-            protoc-gen-grpc-swift
-          ];
-        };
-      });
+    };
 }
